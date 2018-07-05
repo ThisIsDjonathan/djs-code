d4cb7afc2a04a6a203f52baad8edb258486baf01
Djonathan <odjonathankrause@gmail.com>
2018-07-05 20:38:02 -0300

    fixed

--- p5/PerlinNoise/PerlinNoise.js
+++ p5/PerlinNoise/PerlinNoise.js
@@ -1,31 +1,42 @@
-var rows, cols;
 var xoff = 0;
 var yoff = 0;
-var zoff = 100;
-var scl = 80;
-var vel = 0.0002;
+var coff = {r: 13, g: 140, b: 200};
+var force, x, y, color;
 
 function setup() {
-    createCanvas(600, 400);
-    rows = height; // y
-    cols = width;  // x
-    background(50);
+	createCanvas(800, 600);
+	background(51);
 }
 
 function draw() {
-    //background(50);
+   background(255);
 	
-    for (let y = 0; y < rows + scl; y += scl) {
-        for (let x = 0; x < cols + scl; x += scl) {
-            let xx = map(noise(xoff), 0, 1, 0, cols);
-            let yy = map(noise(yoff), 0, 1, 0, rows);
+	color = {r: noise(coff.r) * 100, g: noise(coff.g) * 100, b: noise(coff.b) * 100}	;
+	
+	x = noise(xoff) * width;
+	y = noise(yoff) * height;
+	
+	noStroke();
+	fill(color.r, color.g, color.b);
+	ellipse(x, y, 70, 70);
+	
+	xoff += 0.01;
+	yoff += 0.005;
+	coff.r += 0.01;
+	coff.g += 0.01;
+	coff.b += 0.01;
 
-            stroke(255, 255, 255, 100);
-            point(xx, yy);
-            xoff += vel;
-      }
-      yoff += vel;      
-    }
-
-    console.log(frameRate());
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
