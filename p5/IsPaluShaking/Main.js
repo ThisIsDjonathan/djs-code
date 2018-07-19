47d4698eb5ed5ac8fbfa2e48ba9a54f95f3d8493
Djonathan <odjonathankrause@gmail.com>
2018-07-18 22:24:47 -0300

    working with json and watching just earthwuakes just in japan

--- p5/IsPaluShaking/Main.js
+++ p5/IsPaluShaking/Main.js
@@ -1,100 +1,146 @@
-// https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+/**
+ * Japan's earthquakes visualisation by Djonathan Krause 2018/2. 
+ * Based on Dan Shiffman's tutorial at The Coding Train https://youtu.be/ZiYdOwOrGyc
+ * 
+ * Run with a Python3 server: 
+ * cd to project's folder and python -m http.server
+ */
 
-let MAPBOX_TOKEN = "pk.eyJ1Ijoib2Rqb25hdGhhbmtyYXVzZSIsImEiOiJjampvdndzOGcwNTN1M3ZwMHlnMHY0MWppIn0.hzoLpCR-_bD-5gQxF3-1ag"; 
-let MAP_BASE = "https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/";
-let WIDTH = 800;
-let HEIGHT = 600;
-let zoom = 1;
-let scl = 10;
+// Params
+let MAPBOX_TOKEN = 'pk.eyJ1Ijoib2Rqb25hdGhhbmtyYXVzZSIsImEiOiJjampvdndzOGcwNTN1M3ZwMHlnMHY0MWppIn0.hzoLpCR-_bD-5gQxF3-1ag'
+let MAP_BASE = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/'
+let EARTHQUAKES_DATA_JSON = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=35.359384&longitude=136.612750&maxradiuskm=300&minmagnitude=0'
+let WIDTH = 800
+let HEIGHT = 600
+let zoom = 5
+let scl = 10
 
-let earthquakes;
-let center;
+// Globals 
+let center
+let earthquakes = []
+let earthquakesRaw
 
 let ogaki = {
-    coord: {
-      lat: 35.359384,
-      lon: 136.612750
-    }
-  };
-              
-
-function preload() {  
-  mapimg = loadImage(MAP_BASE + 0 + ',' + 0 + ',' + zoom + '/' 
-                   + WIDTH + 'x' + HEIGHT + '?access_token=' + MAPBOX_TOKEN);
-  //earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv");
-  earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv");
+  lat: 35.359384,
+  lon: 136.612750,
+  pos: {}
 }
 
-
-function setup() {
-  createCanvas(WIDTH, HEIGHT);
-  background(0);
-  translate(width / 2, height / 2);
-  imageMode(CENTER);
-  image(mapimg, 0, 0);
-  
-  center = calcMercator({lat: 0, lon: 0});
-  let pos = calcMercator(ogaki.coord);
-
-  noStroke();
-  fill(255, 0, 199, 100);
-  ellipse(pos.x - center.x, pos.y - center.y, 20, 20);
-
-  sclSlider = createSlider(10, 100, 1);
-
-  drawEarthquakes();
+let Earthquake = function (lat, lon, pos, mag) {
+  this.lat = lat
+  this.lon = lon
+  this.pos = pos
+  this.mag = mag
 }
-
-function draw() {
-  if(sclSlider.value() != scl) {
-    scl = sclSlider.value();
-    drawEarthquakes();
-  }
-}
-
-
-function drawEarthquakes() {
-  // Load data from usgs earthquake API
-  for (let i = 1; i < earthquakes.length; i++) {
-    // Split screen on comma
-    let data = earthquakes[i].split(/,/);
-    
-    // Get earthquake's coordinates and magnitude
-    let earthquake = {coord: {lat: data[1],
-                              lon: data[2] 
-                            },
-                      mag: data[4]
-                    };
-    
-    // Mercator projection of earthquake's coords
-    let pos = calcMercator(earthquake.coord);
-
-    // Scale magnitude to draw a proportional ellipse
-    let magnitude = pow(10, earthquake.mag);
-    magnitude = sqrt(magnitude);
-    let magmax = sqrt(pow(10, 10));
-    let d = map(magnitude, 0, magmax, 0, 180) * scl;
-
-    // Draw ellipse on earthquake position
-    noStroke();
-    fill(92, 147, 249, 200);
-    ellipse(pos.x - center.x, pos.y - center.y, d, d);
-  }
-}
-
 
 /**
- * https://en.wikipedia.org/wiki/Web_Mercator
- * @param {*} coord 
+ * Load map image and earthquakes from APIs.
  */
-function calcMercator(coord) {
-  let x = ((256 / PI) * pow(2, zoom)) * (radians(coord.lon) + PI);
-  
-  let a = ((256 / PI) * pow(2, zoom));
-  let b = tan(PI / 4 + radians(coord.lat) / 2);
+function preload () {
+  // Load map image from mapbox.com API
+  mapimg = loadImage(MAP_BASE + ogaki.lon + ',' + ogaki.lat + ',' + zoom + '/' + WIDTH + 'x' + HEIGHT + '?access_token=' + MAPBOX_TOKEN)
+
+  // Load json with earthquake data from USGS Earthquake Hazards Program API
+  let request = new XMLHttpRequest()
+  request.open('GET', EARTHQUAKES_DATA_JSON)
+  request.responseType = 'json'
+
+  request.onload = () => {
+    earthquakesRaw = request.response.features
+    transformData(earthquakesRaw)
+  }
+
+  request.send()
+}
+
+/**
+ * Setup function create canvas and set map imagen translanting it to center.
+ * Also calculate ogaki cartesian's position and draw an ellipse in this position.
+ */
+function setup () {
+  // Create canvas and draw map
+  createCanvas(WIDTH, HEIGHT)
+  background(0)
+  translate(width / 2, height / 2)
+  imageMode(CENTER)
+  image(mapimg, 0, 0)
+
+  // Calculate x y of Ogaki
+  center = calcMercator({lat: ogaki.lat, lon: ogaki.lon})
+  ogaki.pos = calcMercator({lat: ogaki.lat, lon: ogaki.lon})
+
+  // Draw ellipse on ogaki's position
+  noStroke()
+  fill(255, 0, 199, 100)
+  ellipse(ogaki.pos.x - center.x, ogaki.pos.y - center.y, 20, 20)
+
+  // Draw area
+  fill(210, 212, 216, 10)
+  ellipse(ogaki.pos.x - center.x, ogaki.pos.y - center.y, 500, 500)
+
+  // Draw earthquakes
+  drawEarthquakes()
+}
+
+/**
+ * This function draw earthquakes based on data on earthquakes array.
+ */
+function drawEarthquakes () {
+  if (earthquakes.length > 0) {
+    for (earthquake of earthquakes) {
+      // Scale magnitude to draw a proportional ellipse with some magical math
+      let magnitude = pow(10, earthquake.mag)
+      magnitude = sqrt(magnitude)
+      let magmax = sqrt(pow(10, 10))
+      let d = map(magnitude, 0, magmax, 0, 180) * scl;
+
+      if(d > 3) {
+        fill(244, 66, 66, 200);
+      } else {
+        fill(92, 147, 249, 200);
+      }
+
+      // Draw ellipse on earthquake position
+      noStroke();
+      ellipse(earthquake.pos.x - center.x, earthquake.pos.y - center.y, d, d);
+    }
+  }
+}
+
+/**
+ * This transform raw data (earthquake.usgs.gov json) to a Earthquake object and add it to earthquakes array.
+ * Set earthquake's lat, lon, pos (cartesian) and magnitude.
+ * @param {earthquake.usgs.gov json} raw 
+ */
+function transformData (raw) {
+  for (earthquake of raw) {
+    // Create a new Earthquake object
+    let e = new Earthquake(earthquake.geometry.coordinates[1],
+      earthquake.geometry.coordinates[0],
+      null,
+      earthquake.properties.mag)
+
+    // Mercator projection of earthquake's coords
+    e.pos = calcMercator({lat: e.lat, lon: e.lon})
+
+    // Add it to earthquakes array
+    earthquakes.push(e)
+  }
+}
+
+/**
+ * Transform coordinate position to cartesian's with web mercator: https://en.wikipedia.org/wiki/Web_Mercator
+ * @param {lat, lon object} coord 
+ * @return p5 vector with xy position
+ */
+function calcMercator (coord) {
+  let x = ((256 / PI) * pow(2, zoom)) * (radians(coord.lon) + PI)
+
+  let a = ((256 / PI) * pow(2, zoom))
+  let b = tan(PI / 4 + radians(coord.lat) / 2);   
   let c = PI - log(b);
   let y = a * c;
 
-  let pos = createVector(x, y);
-  return pos;
+  let pos = createVector(x, y)
+  return pos
 }
